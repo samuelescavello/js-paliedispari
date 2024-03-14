08a1726a2e04ca7fca90fccbe82268a32f3209b8
samuelescavello <scavellosamuele23@gmail.com>
2024-03-14 15:52:09 +0100

    es1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+let inserisci1 = prompt('inserisci una parola'); 
+let giraParola = palindromaCek(inserisci1);
+
+if (inserisci1 === giraParola){
+    console.log('la tua parola é palindroma')
+}else{
+    console.log('la tua parola non é palindroma')
+}
+
+
+function palindromaCek (parola1){
+    let parolaContrario = '';
+    for ( let i = parola1.length - 1 ; i > 0 ; i--){
+        parolaContrario = (parolaContrario + parola1[i])
+    }
+    return parolaContrario;
+}
